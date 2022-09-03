97f2c2935d464350ad91032a71b4801f3ea219a1
elias7991 <78571779+elias7991@users.noreply.github.com>
2022-09-02 20:53:42 -0400

    reestructuracion del layout de la app

--- app.py
+++ app.py
@@ -1,69 +1,59 @@
 # importacion de librerias para construir la gui en el navegador
 import dash
 from dash import html, dcc
+from dash.dependencies import Input, Output, State
+
+# estilos que serán utilizados
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # aqui creamos una instancia de la aplicación para ir trabajando
 app = dash.Dash(
     __name__,
-    meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
+    # indicamos el path de donde consultar los estilos
+    external_stylesheets=external_stylesheets,
 )
 
 # aqui le damos un título a la aplicación
 app.title = 'TSP-Resolucion'
 
 # definicion de funciones útiles
-def app_title():
+
+# título de la aplicacion
+def titulo():
     return html.H3(
         'Problema del vendedor viajero',
         style={
-            'textAlign':'center'
+            'textAlign': 'center'
         }
     )
 
-def tabulaciones():
-    return html.Div(
-        id='tabs',
-        className='tabs',
-        children=[
-            dcc.Tabs(
-                id="app-tabs",
-                value="tab2",
-                className="custom-tabs",
-                children=[
-                    dcc.Tab(
-                        id="Specs-tab",
-                        label="Parámetros del problema",
-                        value="tab1",
-                        className="custom-tab",
-                        selected_className="custom-tab--selected",
-                    ),
-                    dcc.Tab(
-                        id="Control-chart-tab",
-                        label="Muestra de resultados",
-                        value="tab2",
-                        className="custom-tab",
-                        selected_className="custom-tab--selected",
-                    ),
-                ],
-            )
-        ]
-    )
+# obtiene los parametros de entrada para generar los gráficos
+def parametros():
+    return
 
 # aqui definimos el diseño de la aplicacion
 app.layout = html.Div(
     children=[
         # le daremos un título general al cuerpo de la aplicacion
-        app_title(),
-        html.Div(
-            id="app-container",
+        titulo(),
+        # aqui solicitaremos los parámetros para dar una solucion al problema
+        parametros(),
+        # tabs que contendran cada una de las soluciones
+        dcc.Tabs(
+            id='tabs',
+            # valor por defecto
+            value='tab1',
             children=[
-                tabulaciones(),
-                # Main app
-                html.Div(id="app-content"),
-            ],
+                dcc.Tab(label='Solución 1', value='tab-1'),
+                dcc.Tab(label='Solución 2', value='tab-2'),
+                dcc.Tab(label='Solución 3', value='tab-3'),
+                dcc.Tab(label='Comparaciones', value='tab-4'),
+            ]
         ),
+        html.Div(id='contenido-tab')
     ]
 )
+
 
 # con esto podemos levantar la aplicación en el navegador
 if __name__ == "__main__":
